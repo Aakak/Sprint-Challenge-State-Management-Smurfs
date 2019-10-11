ba0690d7350bbd651adee49752cc5151f9110874
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-10-11 08:30:16 -0700

    Created folders and modified index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>,
+rootElement
+);
+
